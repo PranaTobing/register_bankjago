b9b6f3e8b9367b5368630e274ddddfacd3826183
Prana Tobing <prana.tobing@gmail.com>
2021-03-17 07:48:32 +0700

    add constants and config size

---
+++ prana_registerform/lib/konstanta.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF4E7FE6);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kButtonColor = Color(0xFF7c9fe3);
+
+const kAnimationDuration = Duration(milliseconds: 200);
